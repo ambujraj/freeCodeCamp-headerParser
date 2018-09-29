375aa394682b2f4a64370f10a0d1ab90c723a3d2
Ambuj <thecoderguy@outlook.com>
2018-09-29 19:49:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-
+app.get("/api/whoami", function(req, res){
+  res.json({ipaddress: req.ip, language: req.headers.accept-language, software: req.header.user-agent});
+});
 
 
 // listen for requests :)
